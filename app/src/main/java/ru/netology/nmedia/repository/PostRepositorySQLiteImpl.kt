e3966629d88d103b38faa9327340e100728047cb
Dm Kv <demon1st@yandex.ru>
2026-05-04 23:08:02 +0300

    task sql_1

---
+++ app/src/main/java/ru/netology/nmedia/repository/PostRepositorySQLiteImpl.kt
@@ -0,0 +1,65 @@
+package ru.netology.nmedia.repository
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.google.gson.Gson
+import ru.netology.nmedia.dao.PostDao
+import ru.netology.nmedia.dto.Post
+
+class PostRepositorySQLiteImpl(
+    private val dao: PostDao
+) : PostRepository {
+
+    private var posts = emptyList<Post>()
+    private var nextId = (posts.maxByOrNull { it.id }?.id ?: 0L) + 1L
+
+    private val data = MutableLiveData(posts)
+
+    init {
+        posts = dao.get()
+        data.value = posts
+    }
+
+    override fun get(): LiveData<List<Post>> = data
+
+    override fun save(post: Post) {
+        posts = if (post.id == 0L) {
+            listOf(post.copy(id = ++nextId, author = "DK", published = "29.03.26")) + posts
+        } else {
+            posts.map {
+                if (it.id == post.id) {
+                    it.copy(content = post.content)
+                } else {
+                    it
+                }
+            }
+        }
+        data.value = posts
+    }
+
+    override fun likeById(id: Long) {
+        dao.likeById(id)
+        posts = posts.map {
+            if (it.id != id) it else it.copy(
+                likedByMe = !it.likedByMe,
+                likes = if (it.likedByMe) it.likes - 1 else it.likes + 1
+            )
+        }
+        data.value = posts
+    }
+
+    override fun shareById(id: Long) {
+        posts = posts.map {
+            if (it.id != id) it else it.copy(shares = it.shares + 1)
+        }
+        data.value = posts
+    }
+
+    override fun removeById(id: Long) {
+        dao.removeById(id)
+        posts = posts.filter { it.id != id }
+        data.value = posts
+    }
+
+}
+
